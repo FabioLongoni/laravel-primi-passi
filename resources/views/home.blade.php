5ffc402a98252a2b00ff7718e82e804cd7372193
FabioLongoni <longoni.fabio90@gmail.com>
2022-10-06 16:07:43 +0200

    add @forelse

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,93 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  <h1>Hello World</h1>
-</body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+            
+            li {
+              list-style: none;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+          <div class="content">
+            <div class="title m-b-md">
+              Buongiorno {{$name}} {{$surname}}
+            </div>
+            <div>
+              questi frameworks devi saperli usare assolutamente
+              <ul>
+                @forelse ($frameworks_list as $framework)
+                    <li>{{$framework}}</li>
+                @empty
+                    <li>nessun framework presente</li>
+                @endforelse
+              </ul>
+            </div>
+            <div class="links">
+              <a href="https://laravel.com/docs">Laravel</a>
+              <a href="https://vuejs.org/">Vue</a>
+            </div>
+          </div>
+        </div>
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+      'name' => 'Fabio',
+      'surname' => 'Longoni',
+      'frameworks_list' => [
+        'Vue',
+        'Laravel'
+      ]
+      ];
+    return view('home',$data);
 });
